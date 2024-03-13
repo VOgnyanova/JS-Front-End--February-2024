65feec0b8270d26a2e68f6f4d5d8b97073f2c9a3
V.Ognyanova <103129757+VOgnyanova@users.noreply.github.com>
2024-03-13 21:33:50 +0200

    Objects and Classes - Lab

---
+++ Objects and Classes - Lab/01. Person Info.js
@@ -0,0 +1,13 @@
+function solve(firstName, lastName, age) {
+    const personInfo = {
+        firstName,
+        lastName,
+        age,
+    };
+
+    return personInfo;
+}
+solve("George", 
+"Smith",
+"18"
+);
