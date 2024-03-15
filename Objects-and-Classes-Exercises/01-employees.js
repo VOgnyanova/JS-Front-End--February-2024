97f3c733539e64b0ffa6f3dd13a6bd107af795ae
V.Ognyanova <103129757+VOgnyanova@users.noreply.github.com>
2024-03-15 21:08:24 +0200

    Objects and Classes - Exercises

---
+++ Objects-and-Classes-Exercises/01-employees.js
@@ -0,0 +1,19 @@
+function solve(employeeNames) {
+    const employees = {};
+
+    for (const name of employeeNames) {
+        employees[name] = name.length
+    }
+
+    for (const employee in employees) {
+        console.log(`Name: ${employee} -- Personal Number: ${employees[employee]}`);
+    }
+}
+
+
+fanciestSolve([
+    'Silas Butler',
+    'Adnaan Buckley',
+    'Juan Peterson',
+    'Brendan Villarreal'
+])
