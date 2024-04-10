026aa88846abe2627b68bcf0cdb5967835d98453
V.Ognyanova <103129757+VOgnyanova@users.noreply.github.com>
2024-04-10 16:15:53 +0300

    DOM Manipulation and Events Lab

---
+++ DOM Manipulation and Events Lab/02. Delete-From-Table/app.js
@@ -0,0 +1,22 @@
+function deleteByEmail() {
+    const inputEmailElement = document.querySelector('input[name=email]');
+    const resultElement = document.getElementById('result');
+    const trElements = document.querySelectorAll('table#customers tbody tr')
+
+    
+    const resultTrElement = Array
+        .from(trElements)
+        .find(trElement => trElement.children[1].textContent.includes(inputEmailElement.value))
+
+    if (resultTrElement) {
+    
+        resultTrElement.remove();
+      
+        resultElement.textContent = 'Deleted.';
+    } else {
+        resultElement.textContent = 'Not found.';
+    }
+
+    inputEmailElement.value = '';
+}
+
